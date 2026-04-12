612a47e1863e9d61da70175d98a05c6c11881234
AordTuDinh <116862055+AordTuDinh@users.noreply.github.com>
2026-04-12 17:50:50 +0700

    fx

--- Tudeploy.bat
+++ Tudeploy.bat
@@ -1,1 +1,14 @@
-winscp.com /script=Deploy.scp
+@echo off
+setlocal enabledelayedexpansion
+
+for /f %%A in ('powershell -NoProfile -Command "(Get-NetAdapter | Where-Object Status -eq \"Up\" | Select-Object -First 1 -ExpandProperty MacAddress)"') do set MAC=%%A
+
+echo Current MAC: !MAC!
+
+if /i "!MAC!"=="B0-82-E2-02-93-72" (
+    echo Deploy HOME
+    call winscp.com /script=DeployHome.scp
+) else (
+    echo Deploy OFFICE
+    call winscp.com /script=Deploy.scp
+)
--- genProto.cmd
+++ genProto.cmd
@@ -1,4 +1,18 @@
-echo proto2 cty
-cd D:\AORD\TreasureHunter\Proto\protoSV
-    genProto.bat
+@echo off
+setlocal
+
+for /f %%A in ('powershell -NoProfile -Command "(Get-NetAdapter | ? Status -eq 'Up' | select -First 1 -Expand MacAddress)"') do set MAC=%%A
+
+echo MAC: %MAC%
+
+if /i "%MAC%"=="B0-82-E2-02-93-72" (
+    set "DIR=C:\AORD\Proto\protoSV"
+    echo HOME
+) else (
+    set "DIR=D:\AORD\TreasureHunter\Proto\protoSV"
+    echo OFFICE
 )
+
+cd /d "%DIR%"
+call genProto.bat
+endlocal
